04a6a78d4e22feae3ad0ca4c41919653e4379ac4
JongSoo Lee <dldnxks12@naver.com>
2021-08-08 17:20:37 +0900

    Update Softmax_low.py

--- DeepLearning/Pytorch/MultiClass_Classification/Softmax_low.py
+++ DeepLearning/Pytorch/MultiClass_Classification/Softmax_low.py
@@ -14,17 +14,24 @@ import torch
 import torch.nn.functional as F
 
 z = torch.rand((3,5), requires_grad=True) # 3 x 5 random value tensor define
-print(z)
-
 hypothesis = F.softmax(z, dim = 1) # 각 x_data에 대해서 softmax를 진행하므로 dim = 1로 설정
 
-print(hypothesis) 
-
-# 이제 위 hypothesis를 cost 함수에 넣어준다 그러기 위해선 label이 필요하지?
+# 이제 위 hypothesis와 label을 사용해서 cost를 계산한다 --- onehot 인코딩된 label이 필요!
 
 # print(y) : tensor([4, 0, 1])
 y = torch.randint(5, (3,)).long()  # long type으로 0~5까지 수 중에서 3개 3, size로 
+y = y.unsqueeze(1)
 
+# 사실 y = torch.randint(5, (3,1)).long()으로 해도 상관 x
+
+'''
+
+print(y)
+
+tensor([3,4,3])  --- tensor( [[3],[4],[3]] )
+shape : (3, ) -> (3 x 1) 
+        
+'''
 
 # one-hot encoding 
 y_onehot = torch.zeros_like(hypothesis)  # hypothesis와 크기가 같은 0으로 초기화된 tensor
@@ -39,18 +46,7 @@ tensor([[0., 0., 0., 0., 0.],
 
 # y_onehot
 
-y = y.unsqueeze(1)
-
-'''
-print(y)
-
-tensor([[3],
-        [4],
-        [3]]) --- (3, ) -> (3 x 1) size로 !
-'''
-
 y_onehot.scatter_(1, y, 1) # (dim, 해당하는 y, 해당하는 위치에 1을 넣어라!)
-
 
 '''
 print(y_onehot)
