80d87a66cb5259fd47ef173335e0ee58992b89b3
js <dldnxks12@naver.com>
2021-11-12 01:19:35 +0900

    Term Project

--- ReinforcementLearning/Term Project/Windy Grid World.py
+++ ReinforcementLearning/Term Project/Windy Grid World.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 WORLD_HEIGHT = 7
 WORLD_WIDTH = 10
 
-# Windy Weights along the rows
-WIND = [0,0,0,1,1,1,2,2,1,0]
 
 # Possible Actions at each State
 ACTION_UP = 0
@@ -34,22 +32,46 @@ GOAL = [3, 7]
 
 # Action
 ACTIONS = [ACTION_UP,ACTION_DOWN,ACTION_LEFT,ACTION_RIGHT]
-####################################################################################
 
-# Action
+# Horizontal WIND && VERTICAL WIND
+HOR_WIND = [0,-1,1,1,1,-1,0]
+WIND = [0,0,0,1,1,1,2,2,1,0]
+
 def Step(state, action):
     i, j = state
+
     if action == ACTION_UP:
-        return [max(i-1-WIND[j], 0), j]
+        if HOR_WIND[i] == 1:
+            return [max(i - 1 - WIND[j], 0), min(j + HOR_WIND[i] , WORLD_WIDTH-1)]
+        elif HOR_WIND[i] == -1:
+            return [max(i - 1 - WIND[j], 0), max(j + HOR_WIND[i], 0)]
+        else:
+            return [max(i-1-WIND[j], 0), j]
+
     elif action == ACTION_DOWN:
-        return [max( min(i + 1 - WIND[j], WORLD_HEIGHT-1), 0), j] # WIND에 의해 2칸 위로 올라갈 수도 있으니, 외부에 max가 씌워진다.
+        if HOR_WIND[i] == 1:
+            return [max(min(i + 1 - WIND[j], WORLD_HEIGHT - 1), 0), min(j + HOR_WIND[i] , WORLD_WIDTH-1)]
+        elif HOR_WIND[i] == -1:
+            return [max(min(i + 1 - WIND[j], WORLD_HEIGHT - 1), 0), max(j + HOR_WIND[i], 0)]
+        else:
+            return [max( min(i + 1 - WIND[j], WORLD_HEIGHT-1), 0), j]
+
     elif action == ACTION_LEFT:
-        return [max( i - WIND[j], 0), max(j - 1 , 0)]
+        if HOR_WIND[i] == 1:
+            return [max(i - WIND[j], 0), max(j - 1 + HOR_WIND[i], 0)]
+        elif HOR_WIND[i] == -1:
+            return [max(i - WIND[j], 0), max(j - 1 + HOR_WIND[i] , 0)]
+        else:
+            return [max( i - WIND[j], 0), max(j - 1 , 0)]
     elif action == ACTION_RIGHT:
-        return [max( i - WIND[j], 0), min(j + 1, WORLD_WIDTH - 1)]
+        if HOR_WIND[i] == 1:
+            return [max(i - WIND[j], 0), min(j + 1 + HOR_WIND[i], WORLD_WIDTH - 1)]
+        elif HOR_WIND[i] == -1:
+            return [max(i - WIND[j], 0), min(j + 1 + HOR_WIND[i], WORLD_WIDTH - 1)]
+        else:
+            return [max( i - WIND[j], 0), min(j + 1, WORLD_WIDTH - 1)]
     else:
         assert False
-
 
 # play for an episode
 def episode(q_value):
@@ -81,6 +103,29 @@ def episode(q_value):
     return time
 
 
+def episode_q(q_value):
+
+    time = 0
+    state = START
+    GAMMA = 1
+
+    while state != GOAL:
+
+        if np.random.binomial(1, EPSILON) == 1:
+            action = np.random.choice(ACTIONS)
+        else:
+            values_ = q_value[state[0], state[1], :]
+            action = np.random.choice([action_ for action_, value_ in enumerate(values_) if value_ == np.max(values_)])
+
+        next_state = Step(state, action)
+        q_value[state[0], state[1], action] += ALPHA*(REWARD + GAMMA*np.max(q_value[next_state[0], next_state[1], :]) - q_value[state[0],state[1],action])
+
+        state = next_state
+        time += 1
+
+    return time
+
+
 def figure_6_3():
     q_value = np.zeros((WORLD_HEIGHT, WORLD_WIDTH, 4)) # rows x cols x actions
     episode_limit = 500
@@ -91,12 +136,13 @@ def figure_6_3():
     while ep < episode_limit:
         steps.append(episode(q_value))
         ep += 1
+        print("EP : " , ep)
     steps = np.add.accumulate(steps)
 
     plt.plot(steps, np.arange(1, len(steps) + 1))
     plt.xlabel('Time steps')
     plt.ylabel('Episodes')
-    plt.savefig("./figure_6_3.png")
+    plt.savefig("./figure_Sarsa.png")
     plt.close()
 
     # See Optimla Policy Footage
