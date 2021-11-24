57b7e691b2ce6a82a371d957762877e36c7b9adc
js <dldnxks12@naver.com>
2021-11-25 04:39:50 +0900

    LAB4

--- DataScience/LAB4/Submission2.py
+++ DataScience/LAB4/Submission2.py
@@ -19,7 +19,7 @@ pd.set_option('display.max_colwidth', 100)
 data = pd.read_csv('./data/movie_data.csv', sep = ',')
 
 data = data.sample(frac = 1)
-data = data[:1000]
+data = data[:5000]
 
 #Processing
 stopwords = nltk.corpus.stopwords.words('english')
@@ -45,7 +45,7 @@ rf = RandomForestClassifier()
 param = {'n_estimators' : [10, 150, 300],
          'max_depth' : [30, 60, 90, None]}
 
-gs = GridSearchCV(rf, param, cv = 3, n_jobs = 1)
+gs = GridSearchCV(rf, param, cv = 3, n_jobs = 4)
 gs_fit = gs.fit(X_TFIDF_pd, data['sentiment'])
 result1 = pd.DataFrame(gs_fit.cv_results_).sort_values('mean_test_score', ascending = False)
 
@@ -53,24 +53,28 @@ svc = SVC(kernel = 'rbf' , probability=True)
 param = {'gamma' : [0.1, 0.5, 1],
          'C' : [1, 5, 10]}
 
-gs2 = GridSearchCV(svc, param, cv = 3, n_jobs = 1)
+gs2 = GridSearchCV(svc, param, cv = 3, n_jobs = 4)
 gs_fit2 = gs2.fit(X_TFIDF_pd, data['sentiment'])
 result2 = pd.DataFrame(gs_fit2.cv_results_).sort_values('mean_test_score', ascending = False)
 
 gbc = GradientBoostingClassifier()
 param = {'n_estimators' : [100, 300, 500]}
 
-gs3 = GridSearchCV(gbc, param, cv = 3, n_jobs = 1)
+gs3 = GridSearchCV(gbc, param, cv = 3, n_jobs = 4)
 gs_fit3 = gs3.fit(X_TFIDF_pd, data['sentiment'])
 result3 = pd.DataFrame(gs_fit3.cv_results_).sort_values('mean_test_score', ascending = False)
 
-rf_scores = result1['mean_test_score'][:3]
-svm_scores = result2['mean_test_score'][:3]
-gb_scores = result3['mean_test_score'][:3]
+rf_scores = result1['mean_test_score']
+svm_scores = result2['mean_test_score']
+gb_scores = result3['mean_test_score']
 
-rf_scores = np.array(rf_scores)
-svm_scores = np.array(svm_scores)
-gb_scores = np.array(gb_scores)
+rf_scores = np.array(rf_scores)[:3]
+svm_scores = np.array(svm_scores)[:3]
+gb_scores = np.array(gb_scores)[:3]
+
+print(rf_scores)
+print(svm_scores)
+print(gb_scores)
 
 x = np.array([1,2,3])
 plt.figure()
