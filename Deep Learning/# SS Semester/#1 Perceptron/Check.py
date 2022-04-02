e67c82d07827751959a12e47d80cca3ffe0d9bf2
js <dldnxks12@naver.com>
2022-04-03 02:59:12 +0900

    Perceptron Clear

--- Deep Learning/# SS Semester/#1 Perceptron/Check.py
+++ Deep Learning/# SS Semester/#1 Perceptron/Check.py
@@ -2,9 +2,11 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([0,1], [0, 20]) # [Xmin, Xmax] , [Ymin, Ymax]
-plt.xlim((0,1))
-plt.ylim((0,100))
-plt.xlabel('x1')
-plt.ylabel('x2')
-plt.show()
+X = np.arange(10)
+Y = X.copy()
+print(X)
+print(Y)
+
+print(id(X), id(Y))
+
+
