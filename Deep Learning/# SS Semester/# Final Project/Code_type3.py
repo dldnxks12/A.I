46907a39ddc6edbd4bd44ba4b4bbcbc6b7649a43
js <dldnxks12@naver.com>
2022-05-23 23:45:47 +0900

    Final Project

--- Deep Learning/# SS Semester/# Final Project/Code_type3.py
+++ Deep Learning/# SS Semester/# Final Project/Code_type3.py
@@ -62,104 +62,14 @@ class ReplayBuffer():
     def size(self):
         return len(self.buffer)
 
-# Type 2에서는 Node 개수가 다른 Actor를 8개 사용합니다.
+
+# Type 1에서는 동일한 Actor를 8개 사용합니다.
 class MuNet1(nn.Module):
     def __init__(self):
         super(MuNet1, self).__init__()
-        self.fc1 = nn.Linear(24, 128)  # Input  : 24 continuous states
-        self.fc2 = nn.Linear(128, 128)
-        self.fc_mu = nn.Linear(128, 4) # Output : 4 continuous actions
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet2(nn.Module):
-    def __init__(self):
-        super(MuNet2, self).__init__()
-        self.fc1 = nn.Linear(24, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc_mu = nn.Linear(64, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet3(nn.Module):
-    def __init__(self):
-        super(MuNet3, self).__init__()
-        self.fc1 = nn.Linear(24, 256)
+        self.fc1 = nn.Linear(24, 256)  # Input  : 24 continuous states
         self.fc2 = nn.Linear(256, 256)
-        self.fc_mu = nn.Linear(256, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet4(nn.Module):
-    def __init__(self):
-        super(MuNet4, self).__init__()
-        self.fc1 = nn.Linear(24, 96)
-        self.fc2 = nn.Linear(96, 96)
-        self.fc_mu = nn.Linear(96, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet5(nn.Module):
-    def __init__(self):
-        super(MuNet5, self).__init__()
-        self.fc1 = nn.Linear(24, 192)
-        self.fc2 = nn.Linear(192, 192)
-        self.fc_mu = nn.Linear(192, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet6(nn.Module):
-    def __init__(self):
-        super(MuNet6, self).__init__()
-        self.fc1 = nn.Linear(24, 64)
-        self.fc2 = nn.Linear(64, 128)
-        self.fc_mu = nn.Linear(128, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet7(nn.Module):
-    def __init__(self):
-        super(MuNet7, self).__init__()
-        self.fc1 = nn.Linear(24, 64)
-        self.fc2 = nn.Linear(64, 128)
-        self.fc_mu = nn.Linear(128, 4)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        mu = torch.tanh(self.fc_mu(x))
-        return mu
-
-class MuNet8(nn.Module):
-    def __init__(self):
-        super(MuNet8, self).__init__()
-        self.fc1 = nn.Linear(24, 128)
-        self.fc2 = nn.Linear(128, 256)
-        self.fc_mu = nn.Linear(256, 4)
+        self.fc_mu = nn.Linear(256, 4) # Output : 4 continuous actions
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
@@ -169,21 +79,20 @@ class MuNet8(nn.Module):
 
 class QNet1(nn.Module):
     def __init__(self):
-
-        '''
-        TD3 Network를 위해 2개의 Newtork를 생성합니다.
-        '''
-
         super(QNet1, self).__init__()
+
+        '''
+        TD3 Network를 위해 2개의 Newtork를 생성합니다. 
+        '''
         self.fc_sA   = nn.Linear(24, 128)    # State  24 개
         self.fc_aA   = nn.Linear(4, 128)     # Action 4  개
-        self.fc_qA   = nn.Linear(256, 128)   # State , Action 이어붙이기
-        self.fc_outA = nn.Linear(128, 1)     # Output : Q value
+        self.fc_qA   = nn.Linear(256, 256)   # State , Action 이어붙이기
+        self.fc_outA = nn.Linear(256, 1)     # Output : Q value
 
         self.fc_sB   = nn.Linear(24, 128)    # State  24 개
         self.fc_aB   = nn.Linear(4, 128)     # Action 4  개
-        self.fc_qB   = nn.Linear(256, 128)   # State , Action 이어붙이기
-        self.fc_outB = nn.Linear(128, 1)     # Output : Q value
+        self.fc_qB   = nn.Linear(256, 256)   # State , Action 이어붙이기
+        self.fc_outB = nn.Linear(256, 1)     # Output : Q value
 
 
     def forward(self, x, a):
@@ -212,7 +121,6 @@ class OrnsteinUhlenbeckNoise:
         x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
             self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
         self.x_prev = x
-
         x = torch.tensor(x, device = device, dtype = torch.float)
 
         return x
@@ -284,16 +192,15 @@ memory = ReplayBuffer()
 q1 =  QNet1().to(device) # Twin Network for avoiding maximization bias
 q_target1 = copy.deepcopy(q1).eval().to(device) # Parameter Synchronize
 
-
-# 8개의 다른 Actor Network 생성
+# 8개의 동일한 Actor Network 생성
 mu1 = MuNet1().to(device)
-mu2 = MuNet2().to(device)
-mu3 = MuNet3().to(device)
-mu4 = MuNet4().to(device)
-mu5 = MuNet5().to(device)
-mu6 = MuNet6().to(device)
-mu7 = MuNet7().to(device)
-mu8 = MuNet8().to(device)
+mu2 = MuNet1().to(device)
+mu3 = MuNet1().to(device)
+mu4 = MuNet1().to(device)
+mu5 = MuNet1().to(device)
+mu6 = MuNet1().to(device)
+mu7 = MuNet1().to(device)
+mu8 = MuNet1().to(device)
 mu_target1 = copy.deepcopy(mu1).eval().to(device) # Parameter Synchronize
 mu_target2 = copy.deepcopy(mu2).eval().to(device) # Parameter Synchronize
 mu_target3 = copy.deepcopy(mu3).eval().to(device) # Parameter Synchronize
@@ -325,17 +232,16 @@ for m in mu_target8.parameters():
     m.requires_grad = False
 
 # Optimizer 생성 및 하이퍼파라미터 튜닝
-mu_optimizer1 = optim.Adam(mu1.parameters(), lr=0.0009)
-mu_optimizer2 = optim.Adam(mu2.parameters(), lr=0.0005)
-mu_optimizer3 = optim.Adam(mu3.parameters(), lr=0.0003)
-mu_optimizer4 = optim.Adam(mu4.parameters(), lr=0.0001)
-mu_optimizer5 = optim.Adam(mu5.parameters(), lr=0.009)
-mu_optimizer6 = optim.Adam(mu6.parameters(), lr=0.005)
-mu_optimizer7 = optim.Adam(mu7.parameters(), lr=0.003)
-mu_optimizer8 = optim.Adam(mu8.parameters(), lr=0.001)
+mu_optimizer1 = optim.Adam(mu1.parameters(), lr=0.009)
+mu_optimizer2 = optim.Adam(mu2.parameters(), lr=0.005)
+mu_optimizer3 = optim.Adam(mu3.parameters(), lr=0.003)
+mu_optimizer4 = optim.Adam(mu4.parameters(), lr=0.001)
+mu_optimizer5 = optim.Adam(mu5.parameters(), lr=0.09)
+mu_optimizer6 = optim.Adam(mu6.parameters(), lr=0.05)
+mu_optimizer7 = optim.Adam(mu7.parameters(), lr=0.03)
+mu_optimizer8 = optim.Adam(mu8.parameters(), lr=0.01)
 
-q_optimizer1  = optim.Adam(q1.parameters(), lr=0.001)
-
+q_optimizer1  = optim.Adam(q1.parameters(), lr=0.005)
 
 # Noise
 ou_noise = OrnsteinUhlenbeckNoise(mu=np.zeros(4))
@@ -443,8 +349,8 @@ env.close()
 
 # Numpy array로 list 저장
 avg_history = np.array(avg_history)
-np.save("./type2", avg_history)
-np.save("./Softmax type2", softmax_recores)
+np.save("./type3", avg_history)
+np.save("./Softmax type3", softmax_recores)
 
 # Average Reward Plot
 length = np.arange(len(avg_history))
@@ -452,4 +358,4 @@ plt.figure()
 plt.xlabel("Episode")
 plt.ylabel("10 episode MVA")
 plt.plot(length, avg_history)
-plt.savefig('type2.png')
+plt.savefig('type3.png')
