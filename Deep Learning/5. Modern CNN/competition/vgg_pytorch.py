6d76c19adaa12b8aa9eca5168c71e521c73c2f35
Jongsoo Lee <jjongs@postech.ac.kr>
2023-08-17 16:59:20 +0900

    Update vgg_pytorch.py

--- Deep Learning/5. Modern CNN/competition/vgg_pytorch.py
+++ Deep Learning/5. Modern CNN/competition/vgg_pytorch.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, math
 import numpy as np
 from skimage import io
 from network import *
@@ -16,9 +16,6 @@ def main():
     # training and evaluation device
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    start_time = datetime.now().replace(microsecond=0)
-    start_time_str = start_time.strftime("%y-%m-%d-%H-%M-%S")
-
     # TODO : saves model in this directory
     save_dir = './result'
     if not os.path.exists(save_dir):
@@ -35,21 +32,34 @@ def main():
     print("====================================")
 
     # TODO : Make train dataset
-    x_train = []
-    y_train = []
-    for folder_idx in range(1, 1001):
-        for img_idx in range(0, 20):
-            x_train.append(get_image('fake_data_1000cls/'+str(folder_idx), str(img_idx)+".png", crop = True))
-            y_train.append(np.array([folder_idx]))
+    col_dir = './Competition/fake_data/*/*.png'
 
-        # We convert data type in dataset class
-        # x_train = torch.FloatTensor(x_train)
-        # y_train = torch.FloatTensor(y_train)
-        break
+    # creating a collection with the available images
+    col = io.imread_collection(col_dir)
+
+    x_train = []
+    max_h = 420
+    max_w = 620
+    for c in col:
+        sh = np.shape(c)
+        pad_h = math.ceil((max_h - sh[0])/2)
+        pad_w = math.ceil((max_w - sh[1])/2)
+        x_train_img = np.pad(c, ((pad_h, pad_h), (pad_w, pad_w), (0, 0)))
+        x_train_img = x_train_img[:420, :620, :]
+        x_train_img = cv2.resize(x_train_img, (180, 180))
+        x_train.append(x_train_img)
+
+    x_train = np.array(x_train)
+
+    y_train = []
+    for folder_idx in range(10, 31):
+        for img_idx in range(0, 200):
+            # x_train.append(get_image('Competition/fake_data_1000cls/'+str(folder_idx), str(img_idx)+".png", crop = True))
+            y_train.append(np.array([folder_idx]))
 
     # TODO : Define Custom Dataset and DataLoader
     dataset           = CustomDataset(x_train, y_train, device)
-    train_data_loader = DataLoader(dataset=dataset, batch_size = 10, shuffle=True, drop_last=True)
+    train_data_loader = DataLoader(dataset=dataset, batch_size = 20, shuffle=True, drop_last=True)
 
     # TODO : Set your hyperparameters and define model, optimizer
     learning_rate   = 0.01
@@ -57,14 +67,17 @@ def main():
     model           = VGG(in_channel=3).to(device)
     optimizer       = optim.SGD(model.parameters(), lr=learning_rate)
 
+    start_time = datetime.now().replace(microsecond=0)
+    start_time_str = start_time.strftime("%y-%m-%d-%H-%M-%S")
+
     # TODO: train your model!
     for epoch in range(training_epochs):
         avg_cost = 0
         for x, y in train_data_loader:
-            out = model(x)                 # Inference
+            out  = model(x)                # Inference
             cost = F.cross_entropy(out, y) # Calculate cost
 
-            # Optimizer your model based on cost
+            # Optimize your model based on backpropagation
             optimizer.zero_grad()
             cost.backward()
             optimizer.step()
@@ -81,8 +94,6 @@ def main():
     print("finished training at: " + end_time_str)
     print("total training time : " + time_elapsed)
     print("====================================")
-
-
 
 if __name__ == '__main__':
     main()
